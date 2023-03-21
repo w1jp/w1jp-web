8b597d257814864b74bb3deb85797d6f34bb226f
Jon Pellant <jon@pellant.com>
2023-03-21 13:45:49 -0400

    Refactored beacon for enQueue and added space support v1.4

--- beacon.js
+++ beacon.js
@@ -1,12 +1,12 @@
-// 171105 ©2017 W1JP, Gatorbot LLC
-// 
+// 171105 ©2017–2023 W1JP, Gatorbot LLC
+// 230315: updated for >ES6 support of private members and methods.
 
 // used to create symbol elements on the tick-scale
 // 0 = end
 // 1 = dit
 // 2 = dah
 const symbolTable = [
-	[' ',[5,0]],
+	[' ',[3,0]],
 	['A',[1,2,0]],
 	['B',[1,1,1,2,0]],
 	['C',[2,1,2,1,0]],
@@ -52,75 +52,78 @@ const symbolTable = [
 ];
 
 class Beacon {
-	constructor (tx=(type)=>{}, 
-		opts={
-			default: 'W1JP',
-			wpm: 12,
-			farnsworth: 20,
-			endDelay: 2000,
-			playing: false,
-		}) 
-	{
-		this.tx = tx
-		this.opts = opts
-		this.queue = []
-		this.iChar = 0
-		this.iSym = 0
-		this.state = 0 // 0: reset, 1: play, 2: pause
+	wpm = 12;
+	farnsworth = 20;
+	endDelay = 2000;
+	playing = false;
+	queue = [];
+	iChar = 0;
+	iSym = 0;
+	state = 0; // 0: reset, 1: play, 2: pause
+	default = "W1JP";
 
-		// Do the setup calcs
-		const wpmPeriod = 60000/this.opts.wpm
-		const farnsPeriod = 60000/this.opts.farnsworth
+	constructor (tx=(type)=>{}, opts={}) {
+		this.setOptions(opts);
+		this.morse = new Map(symbolTable)
+		this.tx = tx;
+		this.reset();
+	}
+	#setTiming(){
+		const wpmPeriod = 60000/this.wpm
+		const farnsPeriod = 60000/this.farnsworth
 		this.tickPeriod = farnsPeriod/50 // ms
 		this.spacePeriod = (wpmPeriod-farnsPeriod)/19 + this.tickPeriod
-		this.morse = new Map(symbolTable)
-		this.reset()
 	}
 	// setDefaultMsg()
 	// deQueue()
-	// enQueue()
+	setOptions(opts) {
+		let _wpm, _farns, _delay, _playing;
+		if (_wpm = Number(opts.wpm)) this.wpm = _wpm;
+		if (_farns = Number(opts.farnsworth)) this.farnsworth = _farns;
+		if (_delay = Number(opts.delay)) this.delay = _delay;
+		if (_playing = Boolean(opts.playing)) this.playing = _playing;
+		this.#setTiming();	
+	}
+	enQueue(str) {
+		let msg = ' '+str;
+		this.queue.push(msg.toUpperCase());
+	}
 	// play()
 	play() {
 		if (this.state == 1) return
-		if (this.queue.length==0) this.queue.push(this.opts.default)
+		if (this.queue.length==0) this.queue.push(this.default)
 		this.state = 1
 		this.timer = window.setTimeout(() => {this.tick()},this.tickPeriod)
 	}
 	// pause()
-	pause () {
+	pause() {
 		if (!this.playing) return
 		window.clearTimeout(this.timer)
 		this.playing = false
 	}
 	// reset()
-	reset () {
-		if (this.queue.length==0) this.queue.push(this.opts.default)
+	reset() {
+		if (this.queue.length==0) this.queue.push(this.default)
 		this.iChar = 0
 		this.iSym = 0
 		if (this.state == 1 && this.timer == 0) 
 			this.timer = window.setTimeout(() => {this.tick()},this.tickPeriod)
 	}
-	// getOpts()
-	// setOpts()
 	// tick()
-	tick ()  {
+	tick()  {
 		// handle tick
 		if (this.state != 1) return
 		let char = this.queue[0][this.iChar]
 		let symbol = this.morse.get(char)[this.iSym]
 		let delay = 0 // used to set next timer
-		if (char == ' ') {
-			delay = 7 * this.wordPeriod - this.tickPeriod
-
-		}
-		else switch (symbol) {
+		switch (symbol) {
 			case 0 :
 				// end symbol space (2 * wordPeriod + (workPeriod-tickPeriod))
 				delay = 3 * this.spacePeriod - this.tickPeriod
 				// get next work
 				if (this.queue[0][++this.iChar]==undefined) { // message done
 					this.queue.splice(0,1) // remove it
-					if (this.queue.length == 0) this.queue.push(this.opts.default) // queue done use default message
+					if (this.queue.length == 0) this.queue.push(this.default) // queue done use default message
 					this.iChar = 0
 					delay = 7 * this.spacePeriod // end of word space
 				}
@@ -138,6 +141,12 @@ class Beacon {
 				delay = this.tickPeriod * 4
 				++this.iSym
 				break
+			// 230321 refactor for enQued messages
+			case 3 :
+				// space
+				delay = 7 * this.wordPeriod - this.tickPeriod;
+				++this.iSym;
+				break;
 			default :
 				throw ('I am confused')
 		}
